8728e5dc43e7be5eb978e77ed78c46ffa6533b76
Benjie Friedman <benjiefriedman@gmail.com>
2021-03-02 22:52:26 -0800

    added more firebase code to stats.html

---
+++ scripts/firebase_api_crowdmapp.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC8eqACcRiyUnY3x4VTuY6xGbQev0Z2dKo",
+    authDomain: "march02demo-abf40.firebaseapp.com",
+    projectId: "march02demo-abf40",
+    storageBucket: "march02demo-abf40.appspot.com",
+    messagingSenderId: "886839076302",
+    appId: "1:886839076302:web:03d60ddd6e1704603d779f",
+    measurementId: "G-LDNVCFK0CL"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
