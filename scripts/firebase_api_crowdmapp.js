66a05ea9ff434e0019582b86b0e59c80b8d6ebca
Benjie Friedman <benjiefriedman@gmail.com>
2021-03-16 22:54:21 -0700

    added team firebase config data to firebase_api_crowdmapp.js

--- scripts/firebase_api_crowdmapp.js
+++ scripts/firebase_api_crowdmapp.js
@@ -1,13 +1,15 @@
-// Your web app's Firebase configuration
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
-    apiKey: "AIzaSyC8eqACcRiyUnY3x4VTuY6xGbQev0Z2dKo",
-    authDomain: "march02demo-abf40.firebaseapp.com",
-    projectId: "march02demo-abf40",
-    storageBucket: "march02demo-abf40.appspot.com",
-    messagingSenderId: "886839076302",
-    appId: "1:886839076302:web:03d60ddd6e1704603d779f",
-    measurementId: "G-LDNVCFK0CL"
+    apiKey: "AIzaSyDH-idNfKi7Fii1zoU4bkniHejbsvckEDg",
+    authDomain: "crowdmapp.firebaseapp.com",
+    projectId: "crowdmapp",
+    storageBucket: "crowdmapp.appspot.com",
+    messagingSenderId: "646550151263",
+    appId: "1:646550151263:web:48fe52fa1bd1ea603b7bea",
+    measurementId: "G-P1GN26RVGM"
   };
+  
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
