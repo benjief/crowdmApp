e293976f8e6fbc325d0f0987aed78f85772c743a
Samuel Tjahjadi <stjahjadi@my.bcit.ca>
2021-03-23 22:14:30 -0700

    combined js. separate js for conflicting files. fixed typos

---
+++ scripts/headcount.js
@@ -0,0 +1,176 @@
+// Head Counter
+var timeout, interval, headcount = 0;
+// Addition
+[].forEach.call(document.querySelectorAll('.add'), function (button) {
+    button.addEventListener('mousedown', function () {
+        document.getElementById("increase_headcount").style.backgroundColor = "green";
+        var x = button.dataset.target;
+        incrementValue(x);
+        timeout = setTimeout(function () {
+            interval = setInterval(function () {
+                incrementValue(x);
+            }, 80); //counting speed set in milliseconds.
+        }, 600); //hold down delay set in milliseconds. 
+    });
+    button.addEventListener('mouseup', clearTimers);
+    button.addEventListener('mouseleave', clearTimers);
+    //stops counter when not pressing the button
+    function clearTimers() {
+        clearTimeout(timeout);
+        clearInterval(interval);
+        document.getElementById("increase_headcount").style.backgroundColor = "transparent";
+    }
+});
+
+function incrementValue(x) {
+    document.getElementById(x).textContent = ++headcount;
+};
+// Substraction
+[].forEach.call(document.querySelectorAll('.subtract'), function (button) {
+    button.addEventListener('mousedown', function () {
+        document.getElementById("decrease_headcount").style.backgroundColor = "red";
+        var y = button.dataset.target;
+        decrementValue(y);
+        timeout = setTimeout(function () {
+            interval = setInterval(function () {
+                decrementValue(y);
+            }, 80); //counting speed set in milliseconds.
+        }, 600); //hold down delay set in milliseconds. 
+    });
+    button.addEventListener('mouseup', clearTimers);
+    button.addEventListener('mouseleave', clearTimers);
+    //stops counter when not pressing the button
+    function clearTimers() {
+        clearTimeout(timeout);
+        clearInterval(interval);
+        document.getElementById("decrease_headcount").style.backgroundColor = "transparent";
+    }
+});
+
+function decrementValue(y) {
+    if (headcount > 0) {
+        document.getElementById(y).textContent = --headcount;
+    };
+};
+var currentUser = "thisisnotworking@bogus.net"; // delete this
+// Add a new update with an auto-generated id when user clicks "Update Headcount" button.
+function onClickUpdate() {
+    var feedback = document.getElementById("feedback");
+    if (!selectedStore || selectedStore === "invalid") {
+        // Display invalid selection prompt if a store isn't selected
+        feedback.innerHTML = "Please select a valid Costco location from the dropdown list.";
+        $(feedback).css({
+            color: "red"
+        });
+        $(feedback).show(0);
+        $(feedback).fadeOut(2500);
+        console.log("No update written");
+    } else {
+        // Get correct store name (in DB format)
+        var storeToUpdate = "Costco_" + selectedStore;
+        // Fetch current user
+        currentUser = firebase.auth().currentUser
+            .email; // delete and change code below (or use variables on other pages)
+        console.log(currentUser); // delete
+        // Get timestamp at time of click
+        var timestamp = firebase.firestore.FieldValue.serverTimestamp(); // delete and change code below?
+        // Get previous headcount
+        var docRef = db.collection("Stores").doc(storeToUpdate).collection("Latest_Update").doc("latest");
+        var previousHeadcount;
+        docRef.get().then((doc) => {
+            if (doc.exists) {
+                previousHeadcount = doc.data()
+                    .Current_Headcount; // Get headcount for entry being replaced
+                // Add update to "Latest_Update" collection
+                db.collection("Stores").doc(storeToUpdate).collection("Latest_Update").doc("latest")
+                    .set({
+                        Current_Headcount: headcount,
+                        Date_Time: timestamp,
+                        Updater: currentUser,
+                        Previous_Headcount: previousHeadcount
+                    })
+                    .then(() => {
+                        console.log("Document successfully written!");
+                        // Get correct day of the week
+                        var weekday;
+                        docRef.get().then((doc) => { // Get the latest update
+                            if (doc.exists) {
+                                weekday = doc.data().Date_Time; // Read the Date_Time entry
+                                weekday = weekday
+                                    .toDate(); // Convert it to a JS date object
+                                weekday = weekday
+                                    .getDay(); // Get the day from this JS date object (0 = Sunday, ..., 6 = Saturday)
+                                switch (weekday) { // Fetch the correct weekday
+                                    case 0:
+                                        weekday = "Sunday";
+                                        break;
+                                    case 1:
+                                        weekday = "Monday";
+                                        break;
+                                    case 2:
+                                        weekday = "Tuesday";
+                                        break;
+                                    case 3:
+                                        weekday = "Wednesday";
+                                        break;
+                                    case 4:
+                                        weekday = "Thursday";
+                                        break;
+                                    case 5:
+                                        weekday = "Friday";
+                                        break;
+                                    case 6:
+                                        weekday = "Saturday";
+                                        break;
+                                }
+                                // Add timestamp to correct day collection
+                                db.collection("Stores").doc(storeToUpdate).collection(
+                                        weekday).add({
+                                        Current_Headcount: headcount,
+                                        Date_Time: timestamp,
+                                        Updater: currentUser,
+                                        Previous_Headcount: previousHeadcount
+                                    })
+                                    .then((docRef) => {
+                                        console.log("Document written with ID: ", docRef
+                                            .id);
+                                        // Display successful update message
+                                        function displaySuccessfulFeedback() {
+                                            feedback.innerHTML = "Update Successful.";
+                                            $(feedback).css({
+                                                color: "green"
+                                            });
+                                            $(feedback).show(0);
+                                            $(feedback).fadeOut(2500);
+                                        }
+                                        displaySuccessfulFeedback();
+                                    })
+                                    .catch((error) => {
+                                        console.error("Error adding document: ", error);
+                                    });
+                            } else {
+                                // doc.data() will be undefined in this case
+                                console.log("No such document!");
+                            }
+                        }).catch((error) => {
+                            console.log("Error getting document:", error);
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("Error writing document: ", error);
+                    });
+            } else {
+                // Add update to "Latest_Update" collection - this code will run if there isn't already an initial update
+                db.collection("Stores").doc(storeToUpdate).collection("Latest_Update").doc("latest")
+                    .set({
+                        Current_Headcount: headcount,
+                        Date_Time: timestamp,
+                        Updater: currentUser
+                    })
+                console.log("An initial 'latest update' has been added!");
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        })
+    }
+}
---
+++ scripts/store_main.js
@@ -0,0 +1,129 @@
+        // Read real-time Burnaby headcount data and write it to the screen
+        function updateHeadcountBurnaby() {
+            db.collection("Stores").doc("Costco_Burnaby").collection("Latest_Update").doc("latest")
+                .onSnapshot((doc) => {
+                    document.getElementById("headcount_burnaby").innerHTML = doc.data().Current_Headcount;
+                    document.getElementById("update_time_burnaby").innerHTML = doc.data().Date_Time.toDate();
+                    if (doc.get("Previous_Headcount") != null) {
+                        document.getElementById("burnaby_change").innerHTML = Math.abs(doc.data()
+                            .Current_Headcount - doc.data().Previous_Headcount);
+                        if (doc.data().Previous_Headcount > doc.data().Current_Headcount) {
+                            document.getElementById("burnaby_ticker").setAttribute("src", "/images/down_arrow.png");
+                        } else if (doc.data().Previous_Headcount < doc.data().Current_Headcount) {
+                            document.getElementById("burnaby_ticker").setAttribute("src", "/images/up_arrow.png");
+                        } else {
+                            document.getElementById("burnaby_ticker").removeAttribute("src");
+                        }
+                    }
+                });
+        }
+
+        // Read real-time Downtown headcount data and write it to the screen
+        function updateHeadcountDowntown() {
+            db.collection("Stores").doc("Costco_Downtown").collection("Latest_Update").doc("latest")
+                .onSnapshot((doc) => {
+                    document.getElementById("headcount_downtown").innerHTML = doc.data().Current_Headcount;
+                    document.getElementById("update_time_downtown").innerHTML = doc.data().Date_Time.toDate();
+                    if (doc.get("Previous_Headcount") != null) {
+                        document.getElementById("downtown_change").innerHTML = Math.abs(doc.data()
+                            .Current_Headcount - doc.data().Previous_Headcount);
+                        if (doc.data().Previous_Headcount > doc.data().Current_Headcount) {
+                            document.getElementById("downtown_ticker").setAttribute("src",
+                                "/images/down_arrow.png");
+                        } else if (doc.data().Previous_Headcount < doc.data().Current_Headcount) {
+                            document.getElementById("downtown_ticker").setAttribute("src", "/images/up_arrow.png");
+                        } else {
+                            document.getElementById("downtown_ticker").removeAttribute("src");
+                        }
+                    }
+                });
+        }
+
+        // Read real-time Richmond headcount data and write it to the screen
+        function updateHeadcountRichmond() {
+            db.collection("Stores").doc("Costco_Richmond").collection("Latest_Update").doc("latest")
+                .onSnapshot((doc) => {
+                    document.getElementById("headcount_richmond").innerHTML = doc.data().Current_Headcount;
+                    document.getElementById("update_time_richmond").innerHTML = doc.data().Date_Time.toDate();
+                    if (doc.get("Previous_Headcount") != null) {
+                        document.getElementById("richmond_change").innerHTML = Math.abs(doc.data()
+                            .Current_Headcount - doc.data().Previous_Headcount);
+                        if (doc.data().Previous_Headcount > doc.data().Current_Headcount) {
+                            document.getElementById("richmond_ticker").setAttribute("src",
+                                "/images/down_arrow.png");
+                        } else if (doc.data().Previous_Headcount < doc.data().Current_Headcount) {
+                            document.getElementById("richmond_ticker").setAttribute("src", "/images/up_arrow.png");
+                        } else {
+                            document.getElementById("richmond_ticker").removeAttribute("src");
+                        }
+                    }
+                });
+        }
+
+        // Get, update and post average ratings for Burnaby
+        function updateRatingBurnaby() {
+            db.collection("Stores").doc("Costco_Burnaby").collection("Reviews")
+                .onSnapshot((querySnapshot) => {
+                    var ratings = [];
+                    querySnapshot.forEach((doc) => {
+                        ratings.push(doc.data().Reviewer_Rating);
+                    });
+                    var sum = 0;
+                    for (var j = 0; j < ratings.length; j++) {
+                        sum += ratings[j];
+                    }
+                    var avgRating = sum / ratings.length;
+                    document.getElementById("burnaby_stars").setAttribute("style", "--rating: " + avgRating.toFixed(
+                        1).toString() + ";");
+                    document.getElementById("current_rating_burnaby").innerHTML = "Current rating: " + avgRating
+                        .toFixed(1);
+                });
+        }
+
+        // Get, update and post average ratings for Downtown
+        function updateRatingDowntown() {
+            db.collection("Stores").doc("Costco_Downtown").collection("Reviews")
+                .onSnapshot((querySnapshot) => {
+                    var ratings = [];
+                    querySnapshot.forEach((doc) => {
+                        ratings.push(doc.data().Reviewer_Rating);
+                    });
+                    var sum = 0;
+                    for (var j = 0; j < ratings.length; j++) {
+                        sum += ratings[j];
+                    }
+                    var avgRating = sum / ratings.length;
+                    document.getElementById("downtown_stars").setAttribute("style", "--rating: " + avgRating
+                        .toFixed(1).toString() + ";");
+                    document.getElementById("current_rating_downtown").innerHTML = "Current rating: " + avgRating
+                        .toFixed(1);
+                });
+        }
+
+        // Get, update and post average ratings for Richmond
+        function updateRatingRichmond() {
+            db.collection("Stores").doc("Costco_Richmond").collection("Reviews")
+                .onSnapshot((querySnapshot) => {
+                    var ratings = [];
+                    querySnapshot.forEach((doc) => {
+                        ratings.push(doc.data().Reviewer_Rating);
+                    });
+                    var sum = 0;
+                    for (var j = 0; j < ratings.length; j++) {
+                        sum += ratings[j];
+                    }
+                    var avgRating = sum / ratings.length;
+                    document.getElementById("richmond_stars").setAttribute("style", "--rating: " + avgRating
+                        .toFixed(1).toString() + ";");
+                    document.getElementById("current_rating_richmond").innerHTML = "Current rating: " + avgRating
+                        .toFixed(1);
+                });
+        }
+
+        // Call functions
+        updateHeadcountBurnaby();
+        updateHeadcountDowntown();
+        updateHeadcountRichmond();
+        updateRatingBurnaby();
+        updateRatingDowntown();
+        updateRatingRichmond();
