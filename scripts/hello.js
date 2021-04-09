76df8bbe1c730e8e5f909ef378dbfb009ceea61f
Benjie Friedman <benjiefriedman@gmail.com>
2021-04-09 03:12:49 -0700

    main.html and css now conforms to chosen format/style.

--- scripts/hello.js
+++ scripts/hello.js
@@ -9,7 +9,7 @@ function sayHello() {
                     // Extract the first name of the user
                     var name = doc.data().Name.split(" ", 1);
                     if (name) {
-                        $(".name-goes-here").html(" " + name);
+                        $(".name-goes-here").html("&nbsp;"+ name);
                       // Display a generic message if no name is entered when signing up
                     } else {
                         $(".name-goes-here").html(" Costco member!");
