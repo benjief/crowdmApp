afaddb71c5620400f5d00d2abdc2ebd648a50780
Benjie Friedman <benjiefriedman@gmail.com>
2021-04-09 04:48:41 -0700

    feedback.html and css now conform to the chosen format/style.

--- scripts/hello.js
+++ scripts/hello.js
@@ -9,10 +9,12 @@ function sayHello() {
                     // Extract the first name of the user
                     var name = doc.data().Name.split(" ", 1);
                     if (name) {
-                        $(".name-goes-here").html("&nbsp;"+ name);
+                        $(".name-goes-here").html(name);
+                        $(".greeting-goes-here").html(name + ", please give us your feedback!");
                       // Display a generic message if no name is entered when signing up
                     } else {
                         $(".name-goes-here").html(" Costco member!");
+                        $(".greeting-goes-here").html("Please give us your feedback!");
                     }
                 });
         }
