3e939892a04bf2e216040762c49fbc892ebcb43b
Benjie Friedman <benjiefriedman@gmail.com>
2021-04-11 13:33:02 -0700

    cleaned up web folders and added special back function to stores.html.

---
+++ scripts/back.js
@@ -0,0 +1,10 @@
+// Go back and refresh the page (fixes errors with floating forms)
+function GoBackWithRefresh(event) {
+    if ('referrer' in document) {
+        // window.location = document.referrer;
+        /* OR */
+        location.replace(document.referrer);
+    } else {
+        window.history.back();
+    }
+}
