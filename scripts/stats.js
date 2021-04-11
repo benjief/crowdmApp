87fb36448920a0ae0a012bff390bc680334729fc
Benjie Friedman <benjiefriedman@gmail.com>
2021-04-10 19:39:29 -0700

    changed the stats table to only show hours Costco is open.

--- scripts/stats.js
+++ scripts/stats.js
@@ -6,7 +6,8 @@ var currentDate = new Date();
 var currentDay = weekdays[currentDate.getDay()];
 $("#current-day").html(currentDay);
 
-var hoursInADay = 24;
+// Costco hours at our locations are 9AM - 8:30PM
+var hoursInACostcoDay = 12;
 // Create empty array to store all update documents pulled from Firestore
 var masterArray = [];
 // Create empty array to store hourly headcount averages
@@ -14,11 +15,11 @@ var hourlyAverages = [];
 // Create an array of hours in the day
 var hoursIn24HrFormat = []
 // Initialize values to "No data"
-for (var i = 0; i < hoursInADay; i++) {
+for (var i = 0; i < hoursInACostcoDay; i++) {
     hourlyAverages[i] = "No data";
 }
 // Initialize values to 0
-for (var i = 0; i < hoursInADay; i++) {
+for (var i = 0; i < hoursInACostcoDay; i++) {
     hoursIn24HrFormat[i] = 0;
 }
 
@@ -26,11 +27,11 @@ for (var i = 0; i < hoursInADay; i++) {
 function resetArrays() {
     masterArray = [];
     // Initialize values to "No data"
-    for (var i = 0; i < hoursInADay; i++) {
+    for (var i = 0; i < hoursInACostcoDay; i++) {
         hourlyAverages[i] = "No data";
     }
     // Initialize values to 0
-    for (var i = 0; i < hoursInADay; i++) {
+    for (var i = 0; i < hoursInACostcoDay; i++) {
         hoursIn24HrFormat[i] = 0;
     }
 }
@@ -73,10 +74,12 @@ function formatTimes() {
     for (var i = 0; i < hoursIn24HrFormat.length; i++) {
         // Format the hour nicely
         var formattedHour;
-        if (i < 10) {
-            formattedHour = "0" + i + ":00";
+        // 09:00
+        if (i == 0) {
+            formattedHour = "0" + (i + 9) + ":00";
+          // All the rest
         } else {
-            formattedHour = i + ":00";
+            formattedHour = (i + 9) + ":00";
         }
         hoursIn24HrFormat.splice(i, 1, formattedHour);
     }
@@ -93,7 +96,7 @@ function eraseInfoFromDom() {
 // Append times and their corresponding headcounts to the DOM
 function appendInfoToDom() {
     formatTimes();
-    for (var i = 0; i < hoursInADay; i++) {
+    for (var i = 0; i < hoursInACostcoDay; i++) {
         var hour = "<tr class='hour-row' id='" + i.toString() + "-row'></tr>";
         var formattedHour = "<td class='hour-data' id='" + i.toString() + "-hour'></td>";
         var headCount = "<td class='headcount-data' id='" + i.toString() + "-headcount'></td>";
